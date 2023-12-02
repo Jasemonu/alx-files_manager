9b09fb41cbc532b02963e650293cf6297136a343
Jasemonu <jasemonu@gmail.com>
2023-12-02 22:42:29 +0000

    solution to task 0 & 1

---
+++ utils/db.js
@@ -0,0 +1,54 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    const dbUrl = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(dbUrl, { useUnifiedTopology: true });
+    this.connected = false;
+
+    this.connect();
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+      this.connected = true;
+    } catch (err) {
+      console.error('Error connecting to MongoDB:', err);
+      this.connected = false;
+    }
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    try {
+      const usersCollection = this.client.db(this.database).collection('users');
+      const usersCount = await usersCollection.countDocuments();
+      return usersCount;
+    } catch (err) {
+      console.error('Error counting users:', err);
+      return -1;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      const filesCollection = this.client.db(this.database).collection('files');
+      const filesCount = await filesCollection.countDocuments();
+      return filesCount;
+    } catch (err) {
+      console.error('Error counting files:', err);
+      return -1;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    // Handling Redis client errors
+    this.client.on('error', (err) => {
+      console.error('Redis error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+// Creating and exporting an instance of RedisClient
+const redisClient = new RedisClient();
+module.exports = redisClient;
