8404ea8abbfc8aa95003fd1b1670b0426ed2c29c
Jasemonu <jasemonu@gmail.com>
2023-12-03 21:00:55 +0000

    UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,7 +5,6 @@ const dbClient = require('../utils/db');
 
 const UsersController = {
   async postNew(req, res) {
-    console.log('Incoming request body:', req.body);
     const { email, password } = req.body;
 
     if (!email) {
@@ -16,30 +15,17 @@ const UsersController = {
       return res.status(400).json({ error: 'Missing password' });
     }
 
-    const usersCollection = dbClient.db.collection('users');
-    const existingUser = await usersCollection.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({ error: 'Already exist' });
+    const checkEmail = await dbClient.checkEmail(email);
+    if (checkEmail !== null && email === checkEmail.email) {
+      return res.status(400).json({ error: 'Already exist' }).end();
     }
-
-    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-
-    const newUser = {
-      email,
-      password: hashedPassword,
-    };
-
-    try {
-      const result = await usersCollection.insertOne(newUser);
-      const { _id, email } = result.ops[0];
-
-      return res.status(201).json({ id: _id, email });
-    } catch (err) {
-      console.error('Error creating user:', err);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
+    const hashedPassword = crypto.createHash('sha1')
+      .update(password)
+      .digest('hex');
+    const user = await dbClient.addUser(email, hashedPassword);
+    return res.status(201).json({ id: user.ops[0]._id, email: user.ops[0].email }).end();
   },
+
 };
 
 module.exports = UsersController;
