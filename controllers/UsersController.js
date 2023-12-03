6392d42f4feaf17f9aa5db7fdaf4b1ebfc249457
Jasemonu <jasemonu@gmail.com>
2023-12-03 14:35:04 +0000

    couple of files

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,60 +1,29 @@
-const { ObjectId } = require('mongodb');
-const crypto = require('crypto');
 const dbClient = require('../utils/db');
 
-const UsersController = {
-  async postNew(req, res) {
+class UsersController {
+  static async postNew(req, res) {
     const { email, password } = req.body;
-
     if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+      res.status(400).json({ error: 'Missing email' });
+      res.end();
+      return;
     }
-
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      res.status(400).json({ error: 'Missing password' });
+      res.end();
+      return;
     }
-
-    const usersCollection = dbClient.db.collection('users');
-    const existingUser = await usersCollection.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({ error: 'Already exist' });
+    const userExist = await dbClient.userExist(email);
+    if (userExist) {
+      res.status(400).json({ error: 'Already exist' });
+      res.end();
+      return;
     }
-
-    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-
-    const newUser = {
-      email,
-      password: hashedPassword,
-    };
-
-    try {
-      const result = await usersCollection.insertOne(newUser);
-      const { _id, email } = result.ops[0];
-
-      return res.status(201).json({ id: _id, email });
-    } catch (err) {
-      console.error('Error creating user:', err);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-  },
-  async getUser(req, res) {
-    const userId = req.params.id;
-
-    if (!ObjectId.isValid(userId)) {
-      return res.status(400).json({ error: 'Invalid user ID' });
-    }
-
-    const usersCollection = dbClient.db.collection('users');
-    const user = await usersCollection.findOne({ _id: ObjectId(userId) });
-
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-    return res.status(200).json({ email: user.email, id: user._id });
-  },
-
-};
+    const user = await dbClient.createUser(email, password);
+    const id = `${user.insertedId}`;
+    res.status(201).json({ id, email });
+    res.end();
+  }
+}
 
 module.exports = UsersController;
