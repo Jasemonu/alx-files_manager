ac6ccf1cbdc3a0d355086c4954805581ebe91bbf
Jasemonu <jasemonu@gmail.com>
2023-12-03 14:32:03 +0000

    solution

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,31 +1,20 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-const AppController = {
-  async getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = dbClient.isAlive();
-
-    if (redisAlive && dbAlive) {
-      return res.status(200).json({ "redis": true, "db": true });
+class AppController {
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.json({ redis: true, db: true });
+      res.end();
     }
-    return res.status(500).json({ "redis": redisAlive, "db": dbAlive });
-  },
+  }
 
-  async getStats(req, res) {
-    try {
-      const usersCount = await dbClient.nbUsers();
-      const filesCount = await dbClient.nbFiles();
-
-      if (usersCount !== -1 && filesCount !== -1) {
-        return res.status(200).json({ users: usersCount, files: filesCount });
-      }
-      return res.status(500).json({ error: 'Failed to retrieve counts' });
-    } catch (err) {
-      console.error('Error getting stats:', err);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-  },
-};
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.json({ users, files });
+    res.end();
+  }
+}
 
 module.exports = AppController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,43 +1,43 @@
 const { ObjectId } = require('mongodb');
-const dbClient = require('../utils/db');
 const crypto = require('crypto');
+const dbClient = require('../utils/db');
 
 const UsersController = {
-    async postNew(req, res) {
-       const { email, password } = req.body;
+  async postNew(req, res) {
+    const { email, password } = req.body;
 
-        if (!email) {
-            return res.status(400).json({ error: 'Missing email' });
-        }
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
 
-        if (!password) {
-            return res.status(400).json({ error: 'Missing password' });
-        }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
 
-        const usersCollection = dbClient.db.collection('users');
-        const existingUser = await usersCollection.findOne({ email });
+    const usersCollection = dbClient.db.collection('users');
+    const existingUser = await usersCollection.findOne({ email });
 
-        if (existingUser) {
-            return res.status(400).json({ error: 'Already exist' });
-        }
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
 
-        const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
 
-        const newUser = {
-            email,
-            password: hashedPassword,
-        };
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
 
-        try {
-            const result = await usersCollection.insertOne(newUser);
-            const { _id, email } = result.ops[0];
+    try {
+      const result = await usersCollection.insertOne(newUser);
+      const { _id, email } = result.ops[0];
 
-            return res.status(201).json({ id: _id, email });
-        } catch (err) {
-            console.error('Error creating user:', err);
-            return res.status(500).json({ error: 'Internal server error' });
-        }
-    },
+      return res.status(201).json({ id: _id, email });
+    } catch (err) {
+      console.error('Error creating user:', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  },
   async getUser(req, res) {
     const userId = req.params.id;
 
