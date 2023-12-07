b346c315e5a42cd28d5efde46dda24d55d28daf2
Jasemonu <jasemonu@gmail.com>
2023-12-07 23:28:08 +0000

    index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -12,5 +13,8 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
