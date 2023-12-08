3ef8147917cf75cff80bc47dfb5f944b4dcc4b0e
Jasemonu <jasemonu@gmail.com>
2023-12-08 03:31:20 -0800

    FilesController.js

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable no-unused-vars */
 mport { tmpdir } from 'os';
 import { promisify } from 'util';
 import Queue from 'bull/lib/queue';
