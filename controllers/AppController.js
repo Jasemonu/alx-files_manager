5594f0d1f058f987e51ba3f2aadee67c0c2035e6
Jasemonu <jasemonu@gmail.com>
2023-12-09 11:46:09 +0000

    AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,31 +1,16 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+// import { app } from '../server';
+import { redisClient } from '../utils/redis';
+import { dbClient } from '../utils/db';
 
-const class AppController = {
-  async getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = dbClient.isAlive();
+module.exports = class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
 
-    if (redisAlive && dbAlive) {
-      return res.status(200).json({ redis: true, db: true });
-    }
-    return res.status(500).json({ redis: redisAlive, db: dbAlive });
-  },
-
-  async getStats(req, res) {
-    try {
-      const usersCount = await dbClient.nbUsers();
-      const filesCount = await dbClient.nbFiles();
-
-      if (usersCount !== -1 && filesCount !== -1) {
-        return res.status(200).json({ users: usersCount, files: filesCount });
-      }
-      return res.status(500).json({ error: 'Failed to retrieve counts' });
-    } catch (err) {
-      console.error('Error getting stats:', err);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-  },
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([users, files]) => {
+        res.status(200).json({ users, files });
+      });
+  }
 };
-
-module.exports = AppController;
