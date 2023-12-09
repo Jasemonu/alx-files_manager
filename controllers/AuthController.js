5d192643022563fe067644c95531fc21a69b4341
Jasemonu <jasemonu@gmail.com>
2023-12-09 11:11:47 +0000

    AuthController.jsu

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-const class AuthController = {
+const AuthController = {
   async getConnect(req, res) {
     const authHeader = req.headers.authorization;
 
@@ -15,16 +15,21 @@ const class AuthController = {
 
     const auth = Buffer.from(authHeader.split(' ')[1], 'base64')
       .toString().split(':');
+    if (auth === null) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
     const password = crypto.createHash('sha1').update(auth[1]).digest('hex');
     const user = await dbClient.checkEmail(auth[0]);
-    if (user.password !== password) {
+
+    if (user === null || user.password !== password) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const token = uuidv4();
     const key = `auth_${token}`;
 
-    redisClient.client.set(key, user._id.toString(), 'EX', 86400);
+    await redisClient.client.set(key, user._id.toString(), 'EX', 86400);
 
     return res.status(200).json({ token });
   },
@@ -43,7 +48,7 @@ const class AuthController = {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    redisClient.client.del(key);
+    await redisClient.del(key);
     return res.status(204).end();
   },
 };
