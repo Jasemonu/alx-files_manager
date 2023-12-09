dabebe761c46adea23d321b33347c45fd1902a2b
Jasemonu <jasemonu@gmail.com>
2023-12-09 10:34:36 +0000

    UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -23,6 +23,7 @@ const class UsersController = {
     const hashedPassword = crypto.createHash('sha1')
       .update(password)
       .digest('hex');
+      console.log(hashedPassword);
     const user = await dbClient.addUser(email, hashedPassword);
     return res.status(201).json({ id: user.ops[0]._id, email: user.ops[0].email }).end();
   },
@@ -31,14 +32,17 @@ const class UsersController = {
     const token = req.headers['x-token'];
 
     if (!token) {
+      console.log('first authorisation error');
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const key = `auth_${token}`;
     const userId = await redisClient.client.get(key);
+    console.log(userId);
     const user = await dbClient.findUserById(userId);
 
     if (!user) {
+      console.log('second authorisation error');
       return res.status(401).json({ error: 'Unauthorized' });
     }
     return res.status(200).json({ email: user.email, id: user._id });
